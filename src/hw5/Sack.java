f728534e093f3cd4047f911c0801717ce92fea7a
Georg Svetlov <gsvetlov@gmail.com>
2021-09-23 18:34:49 +0300

    рюкзак в прямую

--- src/hw5/Sack.java
+++ src/hw5/Sack.java
@@ -1,6 +1,7 @@
 package hw5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Sack {
@@ -18,33 +19,30 @@ public class Sack {
 
     public int getBestValueForVolumeItems() {
         int volumeStep = list.stream().mapToInt(i -> i.getVolume()).min().orElseThrow();
+        int vSize = volume / volumeStep;
 
-        int[][] values = new int[list.size()][volume / volumeStep];
+        int[][] values = new int[list.size()][vSize];
 
-        for (int v = 1; v <= volume / volumeStep; v++) {
-            values[0][v - 1] =
-                    v * volumeStep >= list.get(0).getVolume() ? list.get(0).getValue() : 0;
+        for (int v = 0; v < vSize; v++) {
+            int volumeLimit = (v + 1) * volumeStep;
+            values[0][v] = volumeLimit >= list.get(0).getVolume() ? list.get(0).getValue() : 0;
         }
 
         for (int j = 1; j < list.size(); j++) {
-            for (int v = 1; v <= volume / volumeStep; v++) {
+            for (int v = 0; v < vSize; v++) {
+                int volumeLimit = (v + 1) * volumeStep;
                 Item item = list.get(j);
-                if (item.getVolume() <= v * volumeStep) {
-                    values[j][v - 1] = Integer.max(values[j - 1][v - 1],
-                            item.getValue() + (((v - 1 - item.getVolume() / volumeStep) < 0 ? 0
-                                    : values[j - 1][v - 1 - item.getVolume()])));
+                if (item.getVolume() <= volumeLimit) {
+                    values[j][v] = Integer.max(values[j - 1][v],
+                            item.getValue() + (((v - item.getVolume() / volumeStep) < 0 ? 0
+                                    : values[j - 1][v - item.getVolume() / volumeStep])));
                 } else {
-                    values[j][v - 1] = values[j - 1][v - 1];
+                    values[j][v] = values[j - 1][v];
                 }
             }
         }
 
-        int max = 0;
-        for (int j = 0; j < list.size(); j++) {
-            max = values[j][volume / volumeStep - 1] > max ? values[j][volume / volumeStep - 1]
-                    : max;
-        }
-        return max;
+        return values[list.size() - 1][vSize - 1];
     }
 
 
